9341e985aee836e0be4690565cbb403c12ed0e46
Yaroslav <yariksp@mail.ru>
2020-06-09 22:30:02 +0300

    Lesson_01

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -4,3 +4,16 @@
 составленного из этих отрезков. Если такой треугольник существует,
 то определить, является ли он разносторонним, равнобедренным или равносторонним.
 """
+first_edge = input('введите первый отрезок: ')
+sec_edge = input('введите второй отрезок: ')
+third_edge = input('введите третий отрезок: ')
+
+if first_edge < sec_edge + third_edge or sec_edge < first_edge + third_edge or third_edge < first_edge + sec_edge:
+    if first_edge == sec_edge and sec_edge == third_edge:
+        print(f'Треугольник со сторонами - равносторонний')
+    elif first_edge == sec_edge or sec_edge == third_edge or third_edge == first_edge:
+        print(f'Треугольник - равнобедренный')
+    else:
+        print(f'Треугольник - разносторонний')
+else:
+    print(f'Треугольника - не существует')
