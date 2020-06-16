6a32e5f1b0ded90eba709ff9ddcca45e898b1572
Yaroslav <yariksp@mail.ru>
2020-06-16 23:14:41 +0300

    lesson_03

--- Урок 3.Практическое задание/task_4.py
+++ Урок 3.Практическое задание/task_4.py
@@ -3,3 +3,16 @@
 
 Подсказка: можно применить ф-цию max с параметром key
 """
+my_list = input('Введите исходный массив: ')
+my_list = my_list.split()
+my_list = [int(i) for i in my_list]
+
+counter = [0, 0]
+
+for i in sorted(my_list):
+    cnt = sorted(my_list).count(i)
+    if cnt > counter[1]:
+        counter = [i, cnt]
+
+print(f'В списке {my_list}\n'
+      f'самое частое число {counter[0]}. Оно встречается {counter[1]} раз.')
--- Урок 3.Практическое задание/task_7.py
+++ Урок 3.Практическое задание/task_7.py
@@ -7,3 +7,29 @@
 Наименьший элемент: -86, встречается в этом массиве 1 раз
 Второй наименьший элемент: -73
 """
+from random import randint
+
+my_list = []
+num_elem = int(input('Введите количество элементов в массиве: '))
+for i in range(num_elem):
+    my_list.append(randint(-100, 100))
+print('Исходный массив: {}'.format(my_list))
+if my_list[0] > my_list[1]:
+    ind_min_1 = 0
+    ind_min_2 = 1
+else:
+    ind_min_1 = 1
+    ind_min_2 = 0
+
+for i in range(2, len(my_list)):
+    # print(i,my_list[i])
+
+    if my_list[i] < my_list[ind_min_1]:
+        box = ind_min_1
+        ind_min_1 = i
+        if my_list[box] < my_list[ind_min_2]:
+            ind_min_2 = box
+    elif my_list[i] < my_list[ind_min_2]:
+        ind_min_2 = i
+print('Первый наименьший элемент: {}'.format(my_list[ind_min_1]))
+print('Второй наименьший элемент: {}'.format(my_list[ind_min_2]))
