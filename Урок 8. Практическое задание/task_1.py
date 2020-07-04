78f5a4042118cda1911001368fa8dff43260a37c
Yaroslav <yariksp@mail.ru>
2020-07-04 18:54:56 +0300

    lesson_08

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,22 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+
+def count_sub(my_str):
+    result = set()
+    len_str = len(my_str)
+    for i in range(len(my_str)):
+        for j in range(len_str):
+            if i < j:
+                hash_ = hashlib.sha1(my_str[i:j].encode('utf-8')).hexdigest()
+                if hash_ not in result:
+                    result.add(hash_)
+                    print(f'{my_str[i:j]}')
+        len_str = len(my_str) + 1
+
+    return len(result)
+
+
+my_str = str(input('Введите строку: '))
+print(f'Итог: {count_sub(my_str)} подстрок')
